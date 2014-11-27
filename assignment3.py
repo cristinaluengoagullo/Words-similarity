aab81308c1123d5fa80c990d4c32f8082fc0132f
Javi Navarro <javing89@gmail.com>
2014-11-27 16:07:25 +0100

    Lexicon task

---
+++ assignment3.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+#LAS LINEAS QUE ACABAN CON # SON ELIMINABLES SI NO SE QUIEREN TENER 
+#EN FICHERO EL LEXICON (CON LOS 250 PRIMEROS) Y LEXICONCLEAN (SIN)
+
+import sys, operator
+
+from sys import argv
+from operator import itemgetter
+
+lexicon = {}
+
+def readInput(file):
+	with open(file, 'r') as f:
+		for line in f:
+			count = 0
+			for word in line.split():
+				if count == 0:
+					times = int(word)
+					count += 1
+				else:
+					if lexicon.has_key(word):
+						lexicon[word] += times
+					else:
+						lexicon[word] = times
+	return
+	
+readInput(argv[1])
+lexicon = sorted(lexicon.items(), key = operator.itemgetter(1), reverse = True)
+
+with open('lexicon.txt','wb') as file:					#
+	for key, value in lexicon:							#
+		w = key + '\t' + str(value) + '\n'				#
+		file.write(w)									#
+
+lexiconClean = {}
+
+with open('lexiconClean.txt','wb') as file:				#
+	clean = 0;
+	for key, value in lexicon:
+		if clean > 249:
+			w = key + '\t' + str(value) + '\n'			#
+			file.write(w)								#
+			lexiconClean[key] = value
+		clean += 1
